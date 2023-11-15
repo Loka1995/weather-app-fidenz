01f5d0897d591f7b1e3c636fb2ad41ded41d2cce
Kalana Ekanayake <ekanayakekalana12@gmail.com>
2023-11-15 14:12:45 +0530

    login widget itegrated

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,11 +1,12 @@
 const template = document.createElement('template');
+// const auth0 = require('@auth0/auth0-spa-js');
 template.innerHTML =
     `
     <link rel="stylesheet" href="/css/Login.css">
     <div id="signin-container">
         <img src="/images/main icon.png" alt="weather-app image" id="signin-image">
         <h1 id="signin-heading">Welcome to Weather App</h1>
-        <a href="https://dev-76bkodou00g0wbsb.us.auth0.com"><button type="button" id="signinbtn">Sign in to Weather App</button></a>
+        <button type="button" id="signinbtn">Sign in to Weather App</button>
     </div>`;
 
 export class LoginButton extends HTMLElement {
@@ -14,6 +15,62 @@ export class LoginButton extends HTMLElement {
 
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        //auth0 docs
+        // auth0.createAuth0Client({
+        //     domain: "dev-76bkodou00g0wbsb.us.auth0.com",
+        //     clientId: "ly1z2WsnsV6YJiKmvXztFgCo5Z2iqf1O",
+        //     authorizationParams: {
+        //         redirect_uri: window.location.origin
+        //     }
+        // }).then(async (auth0Client) => {
+        //     // Assumes a button with id "login" in the DOM
+        //     const loginButton = this.shadowRoot.querySelector("#signinbtn");
+
+        //     loginButton.addEventListener("click", (e) => {
+        //         e.preventDefault();
+        //         auth0Client.loginWithRedirect();
+        //     });
+
+        //     if (location.search.includes("state=") &&
+        //         (location.search.includes("code=") ||
+        //             location.search.includes("error="))) {
+        //         await auth0Client.handleRedirectCallback();
+        //         window.history.replaceState({}, document.title, "/");
+        //     }
+
+        //     // Assumes a button with id "logout" in the DOM
+        //     const logoutButton = document.getElementById("logout");
+
+        //     logoutButton.addEventListener("click", (e) => {
+        //         e.preventDefault();
+        //         auth0Client.logout();
+        //     });
+
+        //     const isAuthenticated = await auth0Client.isAuthenticated();
+        //     const userProfile = await auth0Client.getUser();
+
+        //     // Assumes an element with id "profile" in the DOM
+        //     const profileElement = document.getElementById("profile");
+
+        //     if (isAuthenticated) {
+        //         profileElement.style.display = "block";
+        //         profileElement.innerHTML = `
+        //     <p>${userProfile.name}</p>
+        //     <img src="${userProfile.picture}" />
+        //   `;
+        //     } else {
+        //         profileElement.style.display = "none";
+        //     }
+        // });
+        //auth0 docs conclude.
+
+        this.shadowRoot.querySelector("#signinbtn").addEventListener('click', () => {
+            const event = new CustomEvent('login-process');
+            this.dispatchEvent(event);
+        });
     }
 }
 
--- src/js/script.js
+++ src/js/script.js
@@ -13,53 +13,143 @@ import {
 } from './helpers/dataHelper.js';
 import { LoginButton } from './components/Login.js';
 
+const auth0 = require('@auth0/auth0-spa-js');
+
 window.addEventListener("load", () => {
     main();
 });
 
 const main = async function () {
     try {
-        const login = new LoginButton();
-        document.body.insertAdjacentElement('beforebegin', login);
+        // login button to
+        const loginButton = new LoginButton();
+        document.body.insertAdjacentElement('beforebegin', loginButton);
 
-        // const appMain = new AppMain();
-        // document.body.insertAdjacentElement('afterbegin', appMain);
+        auth0.createAuth0Client({
+            domain: "dev-76bkodou00g0wbsb.us.auth0.com",
+            clientId: "ly1z2WsnsV6YJiKmvXztFgCo5Z2iqf1O",
+            authorizationParams: {
+                redirect_uri: window.location.origin
+            }
+        }).then(async (auth0Client) => {
 
-        // const appHeader = new AppHeader();
-        // appMain.shadowRoot.childNodes[3].insertAdjacentElement('afterbegin', appHeader);
+            // Assumes a button with id "login" in the DOM
+            // loka1995 -> I changed the login button to my custom button from LoginButton component.
+            loginButton.addEventListener("login-process", (e) => {
+                e.preventDefault();
+                auth0Client.loginWithRedirect();
+            });
+
+            if (location.search.includes("state=") &&
+                (location.search.includes("code=") ||
+                    location.search.includes("error="))) {
+                await auth0Client.handleRedirectCallback();
+                window.history.replaceState({}, document.title, "/");
+            }
+
+            // Assumes a button with id "logout" in the DOM
+            const logoutButton = document.getElementById("logout");
+
+            logoutButton.addEventListener("click", (e) => {
+                e.preventDefault();
+                auth0Client.logout();
+            });
+
+            const isAuthenticated = await auth0Client.isAuthenticated();
+            const userProfile = await auth0Client.getUser();
+
+            // Assumes an element with id "profile" in the DOM
+            const profileElement = document.getElementById("profile");
+
+            if (isAuthenticated) {
+                profileElement.style.display = "block";
+                profileElement.innerHTML = `
+            <p>${userProfile.name}</p>
+            <img src="${userProfile.picture}" />
+          `;
+                // const appMain = new AppMain();
+                // document.body.insertAdjacentElement('afterbegin', appMain);
+
+                // const appHeader = new AppHeader();
+                // appMain.shadowRoot.childNodes[3].insertAdjacentElement('afterbegin', appHeader);
+
+                // const appFooter = new AppFooter();
+                // document.body.insertAdjacentElement('beforeend', appFooter);
+
+                // let addCity = new AddCityForm();
+                // appHeader.insertAdjacentElement('afterend', addCity);
+
+                // let cityList = new WeatherItemList();
+                // addCity.insertAdjacentElement('afterend', cityList);
+
+                // const cityCodes = await fetchCityCodes();
+                // await cacheData(cityCodes);
+                // const cachedCityWeatherData = await retrieveDataFromCache("cityData");
+                // const cityObjects = createCityObjects(cachedCityWeatherData);
+
+                // cityObjects.forEach(cityObject => {
+                //     let smallCard = new SmallWeatherCard(cityObject);
+                //     let largeCard = new LargeWeatherCard(cityObject);
+                //     smallCard.addEventListener('show-large-card', () => {
+                //         smallCard.hideSmallCardList();
+                //         largeCard.showLargeCard();
+                //     });
+                //     largeCard.addEventListener('show-small-cards', () => {
+                //         largeCard.backBtnHandler();
+                //         smallCard.showSmallCardList();
+                //     })
+                //     cityList.shadowRoot.childNodes[3].appendChild(smallCard);
+                //     appMain.shadowRoot.childNodes[3].insertAdjacentElement('beforeend', largeCard);
+                //     if (Number(localStorage.getItem("stateID")) === largeCard.cardID) {
+                //         smallCard.hideSmallCardList();
+                //         largeCard.showLargeCard();
+                //     }
+                // })
+                
+            } else {
+                profileElement.style.display = "none";
+            }
+        });
+
+        // rest of the flow
+        const appMain = new AppMain();
+        document.body.insertAdjacentElement('afterbegin', appMain);
+
+        const appHeader = new AppHeader();
+        appMain.shadowRoot.childNodes[3].insertAdjacentElement('afterbegin', appHeader);
         
-        // const appFooter = new AppFooter();
-        // document.body.insertAdjacentElement('beforeend', appFooter);
+        const appFooter = new AppFooter();
+        document.body.insertAdjacentElement('beforeend', appFooter);
 
-        // let addCity = new AddCityForm();
-        // appHeader.insertAdjacentElement('afterend', addCity);
+        let addCity = new AddCityForm();
+        appHeader.insertAdjacentElement('afterend', addCity);
 
-        // let cityList = new WeatherItemList();
-        // addCity.insertAdjacentElement('afterend', cityList);
+        let cityList = new WeatherItemList();
+        addCity.insertAdjacentElement('afterend', cityList);
     
-        // const cityCodes = await fetchCityCodes();
-        // await cacheData(cityCodes);
-        // const cachedCityWeatherData = await retrieveDataFromCache("cityData");
-        // const cityObjects = createCityObjects(cachedCityWeatherData);
+        const cityCodes = await fetchCityCodes();
+        await cacheData(cityCodes);
+        const cachedCityWeatherData = await retrieveDataFromCache("cityData");
+        const cityObjects = createCityObjects(cachedCityWeatherData);
 
-        // cityObjects.forEach(cityObject => {
-        //     let smallCard = new SmallWeatherCard(cityObject);
-        //     let largeCard = new LargeWeatherCard(cityObject);
-        //     smallCard.addEventListener('show-large-card', () => {
-        //         smallCard.hideSmallCardList();
-        //         largeCard.showLargeCard();
-        //     });
-        //     largeCard.addEventListener('show-small-cards', () => {
-        //         largeCard.backBtnHandler();
-        //         smallCard.showSmallCardList();
-        //     })
-        //     cityList.shadowRoot.childNodes[3].appendChild(smallCard);
-        //     appMain.shadowRoot.childNodes[3].insertAdjacentElement('beforeend', largeCard);
-        //     if (Number(localStorage.getItem("stateID")) === largeCard.cardID) {
-        //         smallCard.hideSmallCardList();
-        //         largeCard.showLargeCard();
-        //     }
-        // })
+        cityObjects.forEach(cityObject => {
+            let smallCard = new SmallWeatherCard(cityObject);
+            let largeCard = new LargeWeatherCard(cityObject);
+            smallCard.addEventListener('show-large-card', () => {
+                smallCard.hideSmallCardList();
+                largeCard.showLargeCard();
+            });
+            largeCard.addEventListener('show-small-cards', () => {
+                largeCard.backBtnHandler();
+                smallCard.showSmallCardList();
+            })
+            cityList.shadowRoot.childNodes[3].appendChild(smallCard);
+            appMain.shadowRoot.childNodes[3].insertAdjacentElement('beforeend', largeCard);
+            if (Number(localStorage.getItem("stateID")) === largeCard.cardID) {
+                smallCard.hideSmallCardList();
+                largeCard.showLargeCard();
+            }
+        })
     } catch (error) {
         window.alert('Weather data cannot be retrieved!');
         console.error(error);
